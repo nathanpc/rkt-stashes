71d1a9ee7642b7a3e17933e161d9d089e5508041
Nathan Campos <eeepc904@gmail.com>
2013-10-05 21:33:31 -0300

    Improved the open-file procedure

--- stash.rkt
+++ stash.rkt
@@ -9,9 +9,19 @@
 
 (define open-file
   (lambda (filename)
-	(define file (file->string filename
-							   #:mode 'text))
+	(define file "")
+
+	; Check if the file exists.
+	(cond
+	 [(file-exists? filename) ; File exists.
+	  ; Read the contents of the file and return them.
+	  (set! file (file->string filename
+								 #:mode 'text))]
+	 [else ; File could not be found.
+	  ((printf "File \"~a\" doesn't exist~n" filename)
+	   (exit))])
+
 	file))
 
 (define file-contents (open-file "/home/nathanpc/Stashes/FM Stuff.stash"))
-(display file-contents)
+(displayln file-contents)
